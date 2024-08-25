69d543faa966045fde2b0a581ed22fb0021f3fe6
Maureen E <maureenekeugo1@gmail.com>
2024-08-24 20:53:01 -0400

    Added and styled about me and work section

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import '../About/About.css'
+import theme_pattern from '../../assets/assets/theme_pattern.svg'
+import profile_img  from '../../assets/assets/about_profile.svg'
+
+
+const About = () => {
+  return (
+    <div className='about'>
+        <div className="about-title">
+            <h1>About Me</h1>
+            <img src={theme_pattern} alt="" />
+        </div>
+        <div className="about-section">
+
+            <div className="about-left">
+                <img src={profile_img} alt="" />
+            </div>
+
+            <div className="about-right">
+                <div className="about-para">
+                    <p>With three years of experience as a Full Stack Engineer, I’ve successfully collaborated with diverse clients to deliver innovative solutions and build impactful applications, always striving for excellence and client satisfaction.</p>
+                    <p>Driven by a passion for building and architecture, I transitioned into Full Stack Engineering to merge my love for design and problem-solving. </p>
+                </div>
+
+                <div className="about-skills"> 
+                    <div className="about-skill">
+                        <p>React JS</p>
+                        <hr style={{width:'54%'}} />
+                    </div>
+                    <div className="about-skill">
+                        <p>HTML & CSS</p>
+                        <hr style={{width:'60%'}} />
+                    </div>
+                    <div className="about-skill">
+                        <p>Javascript</p>
+                        <hr style={{width:'54%'}} />
+                    </div>
+                    <div className="about-skill">
+                        <p>Node JS</p>
+                        <hr style={{width:'58%'}} />
+                    </div>
+                    <div className="about-skill">
+                        <p>UX and UI</p>
+                        <hr style={{width:'40%'}} />
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div className="about-achievements">
+            <div className="about-achievement">
+                <h1>3+</h1>
+                <p>YEARS of Experience</p>
+            </div>
+            <hr />
+            <div className="about-achievement">
+                <h1>20+</h1>
+                <p>Projects Completed</p>
+            </div>
+            <hr />
+
+            <div className="about-achievement">
+                <h1>10+</h1>
+                <p>Clients</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/Components/MyWork/MyWork.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import '../MyWork/MyWork.css'
+import theme_pattern from '../../assets/assets/theme_pattern.svg'
+import arrow_icon from '../../assets/assets/arrow_icon.svg'
+import mywork_data from '../../assets/assets/mywork_data' 
+
+const MyWork = () => {
+  return (
+    <div className='my-work'>
+        <div className="work-title">
+            <h1>Latest Work</h1>
+            <img src={theme_pattern} alt="" />
+        </div>
+        <div className="work-container">
+            {mywork_data.map((work,index)=>{
+                return  <img key={index} src={work.w_img} alt="" />
+            })}
+
+        </div>
+
+        <div className="work-showmore">
+            <p>Show More</p>
+            <img src={arrow_icon} alt="" />
+        </div>
+        
+    </div>
+  )
+}
+
+export default MyWork
